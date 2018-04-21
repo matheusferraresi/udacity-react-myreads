804efd78d4beccb3b5b057e01755138db76d1cb0
Matheus Ferraresi <matheusferraresi@live.com>
2018-04-21 23:38:17 +0100

    Feat: Passing books from getAll() to the shelves and rendering

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,18 +1,28 @@
-{
   /*
     Used to mount shelves, receive books then call Book Class and mount one LI per book
   */
-}
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Book from './Book';
 
 class BookShelf extends Component {
+  static PropTypes = {
+      books: PropTypes.array.isRequired
+  }
+
   render() {
+    const { books } = this.props
+
     return (
       <ol className="books-grid">
-        <li>
-          SHELF
-        </li>
+        {
+          books.map((book) => (
+            <Book
+              book={ book }
+              key={ book.id }
+            />
+          ))
+        }
       </ol>
     )
   }
--- src/BookShelves.js
+++ src/BookShelves.js
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types';
 import BookShelf from './BookShelf'
 
 class BookShelves extends Component {
+  static PropTypes = {
+    books: PropTypes.array.isRequired
+  }
 
   render() {
     const { books } = this.props
@@ -15,19 +18,22 @@ class BookShelves extends Component {
 
     return (
       <div className="list-books-content">
-        {console.log(this.props.books)}
-        <div>
-          {shelves.map((shelf, index) => {
-            return(
-              <div className="bookshelf">
-                <h2 className="bookshelf-title">{ shelf.title }</h2>
-                <div className="bookshelf-books">
-                  <BookShelf /> { index }
-                </div>
+        {shelves.map((shelf, index) => {
+
+          const booksFromShelf = books.filter( book => book.shelf === shelf.type)
+
+          return(
+            <div className="bookshelf" key={ index }>
+              <h2 className="bookshelf-title">{ shelf.title }</h2>
+              <div className="bookshelf-books">
+                <BookShelf
+                  books={ booksFromShelf }
+                />
               </div>
-            )
-          })}
-        </div>
+            </div>
+          )
+        })}
+
       </div>
     )
   }
